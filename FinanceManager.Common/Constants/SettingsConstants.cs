72e21960e2e3334876808f3e86babd73a876f0e5
Josh Morton <morton@hotmail.co.uk>
2024-04-03 16:54:15 +0100

    [dev] Started setting up TestContainers for Integration testing

--- FinanceManager.Common/Constants/SettingsConstants.cs
+++ FinanceManager.Common/Constants/SettingsConstants.cs
@@ -3,6 +3,7 @@ namespace FinanceManager.Common.Constants;
 public static class SettingsConstants
 {
     public const string PostgresConnection = "PostgresConnection";
+    public const string IntegrationTestConnection = "IntegrationTestConnection";
     public const string JwtSection = "JWT";
     public const string SwaggerSection = "Swagger";
 }
---
+++ FinanceManager.Tests.Integration/IntegrationTestApplicationFactory.cs
@@ -0,0 +1,44 @@
+﻿using FinanceManager.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Testcontainers.PostgreSql;
+
+namespace FinanceManager.Tests.Integration;
+
+public class IntegrationTestApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithExposedPort("7004")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices((_, services) =>
+        {
+            var descriptorType =
+                typeof(DbContextOptions<DataContext>);
+
+            var descriptor = services
+                .SingleOrDefault(s => s.ServiceType == descriptorType);
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+            
+            services.AddDbContext<DataContext>(options => options.UseNpgsql(_dbContainer.GetConnectionString()));
+        });
+    }
+    
+    public Task InitializeAsync()
+    {
+        return _dbContainer.StartAsync();
+    }
+
+    public new Task DisposeAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
+}
